3fa1fb80e521de7bb238003219b6cf055a01425e
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-11-01 14:16:50 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿//*****************************************************************************
+//** 4. Median of Two Sorted Arrays    leetcode                              **
+//*****************************************************************************
+
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    // Ensure binary search is on the smaller array
+    if (nums1Size > nums2Size) {
+        return findMedianSortedArrays(nums2, nums2Size, nums1, nums1Size);
+    }
+    
+    int m = nums1Size;
+    int n = nums2Size;
+    int low = 0, high = m;
+
+    while (low <= high) {
+        int partitionX = (low + high) / 2;
+        int partitionY = (m + n + 1) / 2 - partitionX;
+        
+        int maxX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+        int minX = (partitionX == m) ? INT_MAX : nums1[partitionX];
+        
+        int maxY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+        int minY = (partitionY == n) ? INT_MAX : nums2[partitionY];
+        
+        if (maxX <= minY && maxY <= minX) {
+            // Found the correct partition
+            if ((m + n) % 2 == 0) {
+                return ((double)fmax(maxX, maxY) + fmin(minX, minY)) / 2;
+            } else {
+                return (double)fmax(maxX, maxY);
+            }
+        } else if (maxX > minY) {
+            high = partitionX - 1;
+        } else {
+            low = partitionX + 1;
+        }
+    }
+
+    // If we get here, the input arrays were not sorted as expected
+    fprintf(stderr, "Input arrays are not sorted.\n");
+    exit(EXIT_FAILURE);
+}
+
+int* input(int* size) {
+    printf("Enter size of array: ");
+    scanf("%d", size);
+    
+    if (*size < 0 || *size > 1000) {
+        printf("Please enter a valid number\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int* array = (int*)malloc(*size * sizeof(int));
+    if (!array) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Enter %d numbers: ", *size);
+    for (int i = 0; i < *size; i++) {
+        scanf("%d", &array[i]);
+    }
+
+    return array;
+}
